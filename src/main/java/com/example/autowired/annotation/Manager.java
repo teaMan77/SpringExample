92504f3d684ca34fe5e24e386271133e70291f53
teaMan77 <180627854+teaMan77@users.noreply.github.com>
2024-12-10 20:11:05 +0530

    @Qualifier Annotation

--- src/main/java/com/example/autowired/annotation/Manager.java
+++ src/main/java/com/example/autowired/annotation/Manager.java
@@ -1,6 +1,7 @@
 package com.example.autowired.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,8 +9,8 @@ public class Manager {
 //    @Autowired -- Field Injection
     private Employee employee;
 
-    @Autowired //constructor injection is recommended over field injection for the sake of clarity.
-    public Manager(Employee employee) {
+//    @Autowired //constructor injection is recommended over field injection for the sake of clarity.
+    public Manager(@Qualifier("employee") Employee employee) {
         this.employee = employee;
     }
 
