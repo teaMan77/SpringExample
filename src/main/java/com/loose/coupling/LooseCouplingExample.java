92432f67ad6c71c4528e6417308aef6c21dc78c7
teaMan77 <180627854+teaMan77@users.noreply.github.com>
2024-12-10 18:10:25 +0530

    Applying Inversion of Control(IoC) to loose coupling package

--- src/main/java/com/loose/coupling/LooseCouplingExample.java
+++ src/main/java/com/loose/coupling/LooseCouplingExample.java
@@ -1,17 +1,26 @@
 package com.loose.coupling;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class LooseCouplingExample {
     public static void main(String[] args) {
-        UserDataProvider databaseProvider = new UserDatabaseProvider();
-        UserManager userManagerWithDB = new UserManager(databaseProvider);
-        System.out.println(userManagerWithDB.getUserInfo());
+//        UserDataProvider databaseProvider = new UserDatabaseProvider();
+//        UserManager userManagerWithDB = new UserManager(databaseProvider);
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        UserDataProvider webServiceProvider = new WebServiceDataProvider();
-        UserManager userManagerWithWS = new UserManager(webServiceProvider);
-        System.out.println(userManagerWithWS.getUserInfo());
+        UserManager userManager = (UserManager) context.getBean("userDataManager");
+        System.out.println(userManager.getUserInfo());
 
-        UserDataProvider newDatabaseProvider = new NewDatabaseProvider();
-        UserManager userManagerWithNewDB = new UserManager(newDatabaseProvider);
-        System.out.println(userManagerWithNewDB.getUserInfo());
+//        UserDataProvider webServiceProvider = new WebServiceDataProvider();
+//        UserManager userManagerWithWS = new UserManager(webServiceProvider);
+        userManager = (UserManager) context.getBean("webDataManager");
+        System.out.println(userManager.getUserInfo());
+
+//        UserDataProvider newDatabaseProvider = new NewDatabaseProvider();
+//        UserManager userManagerWithNewDB = new UserManager(newDatabaseProvider);
+        userManager = (UserManager) context.getBean("newDataManager");
+        System.out.println(userManager.getUserInfo());
     }
 }
